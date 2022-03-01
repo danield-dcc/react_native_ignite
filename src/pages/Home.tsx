85c27cd2b5f0b95712323b042465986ec0835990
Daniel Conceição <daniel.colares@gmail.com>
2022-03-01 20:20:23 -0300

    ✨ Add Todo list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,39 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    //TODO - add new task   
+      const info = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false,
+      }
+      //as duas formas estão corretas
+      setTasks(allData => [...allData, info])
+      setTasks([...tasks, info])
+      
+    
+    
+   
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    //const newListOfTasks = [...tasks]
+    const newListOfTasks = tasks.map(task => ({...task}))
+
+    const foundItem = newListOfTasks.find(task => task.id === id)
+    
+    if(!foundItem){
+      return;
+    }
+    foundItem.done = !foundItem.done;
+    setTasks(newListOfTasks)
+    console.log(foundItem)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const newListOfTasks = tasks.filter(task=> task.id !== id)
+    setTasks(newListOfTasks)
   }
 
   return (
